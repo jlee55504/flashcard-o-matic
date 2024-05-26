6951da29f5ba173c009ecac6372a2d40a96f98ec
Jason <jlee55504@gmail.com>
2024-05-26 12:57:15 -0700

    Fixed the 'Flip button' functioning in the handleCardFlip function by using an optional 'default parameter', adding another 'useState variable/function', fixed the cards index issue by using 'if statements' with the proper 'conditionals' and proper 'nesting' in the right places. I also got the file to display the proper message and JSX/React Bootstrap elements if there aren't enough cards in the deck.

--- src/cards/Study.js
+++ src/cards/Study.js
@@ -7,22 +7,19 @@ import add from '../imgs/add.png';
 function Study() {
     const { deckId } = useParams();
     const [selectedDeckName, setSelectedDeckName] = useState("");
-    const [deck, setDeck] = useState([])
     const [selectedDeckCards, setSelectedDeckCards] = useState([]);
     const [currentCardNumber, setCurrentCardNumber] = useState(1);
     const [currentCardIndex, setCurrentCardIndex] = useState(0);
     const [currentCardText, setCurrentCardText] = useState("");
     const [isCardFlipped, setIsCardFlipped] = useState(false);
     const [firstFlip, setFirstFlip] = useState(false);
-    const [sameCard, setSameCard] = useState(true);
+    const [sameCard, setSameCard] = useState(false);
     let buttonsToDisplay;
-    let notEnoughCards;
-    let enoughCards;
+
     const navigate = useNavigate();
     useEffect(() => {
         async function getDeck() {
             const selectedDeck = await readDeck(deckId);
-            setDeck(selectedDeck);
             setSelectedDeckName(selectedDeck.name);
             setSelectedDeckCards(selectedDeck.cards);              
         }
@@ -36,16 +33,12 @@ function Study() {
         
     }, [selectedDeckCards, currentCardIndex]);
 
-
-    const handleCardFlip = () => {
-
-       if (currentCardIndex -1 === selectedDeckCards.length -1) {
+    const handleCardFlip = (dontFlipCard = false) => {
+       if (currentCardIndex -1 === selectedDeckCards.length -1 && dontFlipCard === false) {
         const confirm = window.confirm("Restart cards? \n Click 'cancel' to return to the home page.");
          if (confirm == true) {   
-             setCurrentCardIndex((currentIndex) => currentIndex - currentIndex);
-           // setCurrentCardIndex(0);
-             setCurrentCardNumber((currentCardNumber) => currentCardNumber - currentCardNumber +1);
-             //setCurrentCardNumber(1)
+             setCurrentCardIndex((currentIndex) => currentIndex = 0);
+             setCurrentCardNumber((currentCardNumber) => currentCardNumber = 1);
              navigate(`/decks/${deckId}/study`);
              setIsCardFlipped(false)
              return;
@@ -53,37 +46,53 @@ function Study() {
          else if (confirm == false) navigate("/");
      } 
 
-       if (isCardFlipped === false && firstFlip === false) {
-        setCurrentCardText(selectedDeckCards[currentCardIndex].Back);
-       setCurrentCardIndex((index) => index + 1);
-        setIsCardFlipped(true);
-
-       }
-     else if (isCardFlipped === false && firstFlip === true) {
-
-            setCurrentCardText(selectedDeckCards[currentCardIndex].Back);
-            setCurrentCardIndex((index) => index + 1)
+if (isCardFlipped === false && dontFlipCard === false || isCardFlipped === true
+        && dontFlipCard === true) {
             setIsCardFlipped(true);
-
-        } else if (isCardFlipped === true) {
+            setSameCard(true);
+            if (dontFlipCard === false){ 
+                setCurrentCardText(selectedDeckCards[currentCardIndex].back);
+                setCurrentCardIndex((index) => index + 1);
+            }
+            else if (dontFlipCard === true) {
+                setCurrentCardText(selectedDeckCards[currentCardIndex -1].front);
+                setIsCardFlipped(false);
+            }
+        } else if (isCardFlipped === true && dontFlipCard === false) {
             setCurrentCardText(selectedDeckCards[currentCardIndex].front);
             setIsCardFlipped(false);
+        }else if (isCardFlipped === false && dontFlipCard === true) {
+            setCurrentCardText(selectedDeckCards[currentCardIndex -1].back);
+            setIsCardFlipped(true);
         }
     }
-    
+
     if (isCardFlipped === true) {
-        buttonsToDisplay = <><Button variant='secondary' className='flip-btn' onClick={handleCardFlip} >Flip</Button> <Button variant='primary'
+        buttonsToDisplay = <><Button variant='secondary' className='flip-btn' 
+        onClick={() => {
+            handleCardFlip(true)
+        }
+    }>Flip</Button> <Button variant='primary'
          className='next-btn' onClick={() => {            
             if (firstFlip  === false) setFirstFlip(true);
             setCurrentCardNumber((cardNumber) => cardNumber + 1); 
+           setSameCard(false);         
             handleCardFlip();
-
-        }}>Next</Button></>
+        }
+    }>Next</Button></>
     }
-    else if (isCardFlipped === false) buttonsToDisplay = <Button variant='secondary' className='flip-btn' onClick={() =>{
-        //if (currentCardIndex > 0) setCurrentCardIndex((index) => index + 1)
-        handleCardFlip();
-    }} >Flip</Button>
+    else if (isCardFlipped === false) { 
+        buttonsToDisplay = <Button variant='secondary' className='flip-btn' 
+        onClick={() => {
+       if (sameCard === true) {
+        handleCardFlip(true);
+        return;
+       }
+           else handleCardFlip();
+    }
+}>Flip</Button>
+}
+
     return (
         <div className='study-div'>
             <div className='nav-bar'><Link to="/" className='home-link' >
@@ -92,17 +101,20 @@ function Study() {
             <h1>Study: {selectedDeckName}</h1>
            { selectedDeckCards.length >=3 ? <Card>
               <Card.Body>
-                <Card.Title>Card of {currentCardNumber} of {selectedDeckCards.length}</Card.Title>
+                <Card.Title>Card of {currentCardNumber} of 
+                {selectedDeckCards.length}</Card.Title>
                 <Card.Text> {currentCardText} </Card.Text>
                 {buttonsToDisplay}
               </Card.Body>
             </Card> : <>
                 <h2>NotEnoughCards</h2>
-                <p>You need at least 3 cards to study. There are {selectedDeckCards.length} in this deck.</p>
-                 <Button variant='primary' onClick={() => navigate(`/decks/${deckId}/cards/new`)} > 
-                 <Image src={add} className="add-icon"  />
-                    Add Cards</Button>
-            </>}
+                <p>You need at least 3 cards to study. There are 
+                    {selectedDeckCards.length} in this deck.</p>
+                 <Button variant='primary' 
+                 onClick={() => navigate(`/decks/${deckId}/cards/new`)} > 
+                 <Image src={add} className="add-icon"  />Add Cards</Button>
+            </>
+            }
         </div>
     )
 }
