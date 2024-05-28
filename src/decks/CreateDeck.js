8fd493a6b213d4cf3d28adddfabf8ea76bf1f0ab
Jason <jlee55504@gmail.com>
2024-05-27 21:33:27 -0700

    Space-out the code for better readability, and removed the create-deck-div 'div'.

--- src/decks/CreateDeck.js
+++ src/decks/CreateDeck.js
@@ -1,11 +1,11 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import {Link, useNavigate } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 import './style.css';
 import Image from 'react-bootstrap/Image';
 import home from '../imgs/home.png';
-import { createDeck, updateDeck, listDecks, deleteDeck } from '../utils/api/index';
-import data from '../data/db.json';
+import { createDeck } from '../utils/api/index';
+
 function CreateDeck() {
     /* */
     const navigate = useNavigate();
@@ -28,24 +28,24 @@ function CreateDeck() {
         setDescription("");
     }
     return (
-        <div className='create-deck-div'>
+        <>
             <div className='nav-bar'><Link to="/" className='home-link' >
                 <Image src={home} className='home-icon' />
                 Home</Link> / Create Deck</div>
-        <h1 className='create-deck-h1'>Create Deck</h1>
-        <form onSubmit={handleSubmit}>
-            <label htmlFor='name'>
-                Name
-                <input type='text' name="name" id="name" placeholder='Deck Name' onChange={handleChange} value={name} />
-            </label>
-            <label htmlFor='description'>
-                Description<textarea id="description" name="description" placeholder='Brief description of the deck' onChange={handleChange} value={description} ></textarea>
-            </label>
-            <Button variant='secondary' type='button' className='cancel-btn' onClick={() => navigate("/")}>Cancel</Button>
-            <Button variant='primary' type='submit' className='submit-btn' >Submit</Button>
-        </form>
-        </div>
-    )
+            <h1 className='create-deck-h1'>Create Deck</h1>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor='name'>
+                    Name
+                    <input type='text' name="name" id="name" placeholder='Deck Name' onChange={handleChange} value={name} />
+                </label>
+                <label htmlFor='description'>
+                    Description<textarea id="description" name="description" placeholder='Brief description of the deck' onChange={handleChange} value={description} ></textarea>
+                </label>
+                <Button variant='secondary' type='button' className='cancel-btn' onClick={() => navigate("/")}>Cancel</Button>
+                <Button variant='primary' type='submit' className='submit-btn' >Submit</Button>
+            </form>
+        </>
+    );
 }
 
 export default CreateDeck;
